445bfae753b81354a1760fd78b0008232486c08d
Gabby <gabegabby99@gmail.com>
2021-02-19 16:42:25 -0500

    start file

---
+++ Functions.js
@@ -0,0 +1,15 @@
+/*The includes() method determines whether an array includes 
+a certain value among its entries, returning true or false as 
+appropriate.
+
+Parameters
+valueToFind
+The value to search for.
+fromIndex (Optional)
+The position in this array at which to begin searching for valueToFind.
+
+Return value
+A Boolean which is true if the value valueToFind
+is found within the array (or the part of the array 
+indicated by the index fromIndex, if specified).
+*/
